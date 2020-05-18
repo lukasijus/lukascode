037a6194093c5194767f9aaf00e4e7d9acf0f3f0
Lukas Rimkus <lukasrimkuslg@gmail.com>
2020-05-18 11:50:51 +0300

    nodejs-heroku-starter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
+const path = require('path')
+const hbs = require('hbs')
 const express = require('express')
 const app = express()
 const port = process.env.PORT
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// Define paths for templates
+const publicDirectoryPath = path.join(__dirname, '../public')
+const vuewsPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
+
+// Setup hbs engine with templates
+app.set('view engine', 'hbs')
+app.set('views', vuewsPath)
+hbs.registerPartials(partialPath)
+
+// use public directory and json encoding
+app.use(express.static(publicDirectoryPath))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'This is NodeJs with hbs engine template',
+        name: 'Your name'
+    })
+})
+
+app.get('/about', (req,res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Your name'
+    })
+})
+
+
 app.listen(port, () => console.log(`Example app listening on port port!`))
